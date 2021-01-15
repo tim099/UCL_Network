9485367a22b38d6c51f89cf6eedbd27330d0b08c
tim1125 <tim11251994@gmail.com>
2021-01-15 22:05:40 +0800

    Update UCL_NetworkDemo

--- UCL_NetworkDemoScript/UCL_NetworkDemo.cs
+++ UCL_NetworkDemoScript/UCL_NetworkDemo.cs
@@ -22,7 +22,6 @@ namespace UCL.NetworkLib.Demo {
                 return path;
             }
         }
-
         public string DownloadPath {
             get { return string.Format(UrlPattern, m_TableID, m_SheetID); }
         }
@@ -32,9 +31,9 @@ namespace UCL.NetworkLib.Demo {
         [UCL.Core.ATTR.UCL_RuntimeOnly]
         [UCL.Core.ATTR.UCL_FunctionButton]
         public void WebRequestDemo() {
-            StartCoroutine(WebRequestLoadBundle(DownloadPath, SavePath));
+            StartCoroutine(WebRequestLoad(DownloadPath, SavePath));
         }
-        private IEnumerator WebRequestLoadBundle(string download_path, string save_path) {
+        private IEnumerator WebRequestLoad(string download_path, string save_path) {
             Debug.LogWarning("WebRequestLoad:" + download_path);
             long file_size = 0;
             ///Get Header
@@ -60,7 +59,6 @@ namespace UCL.NetworkLib.Demo {
             while(!request_opt.isDone) {
                 yield return null;
             }
-
             //yield return request_opt;
 
             if(www.isNetworkError || www.isHttpError) {
@@ -78,68 +76,15 @@ namespace UCL.NetworkLib.Demo {
         }
 #if UNITY_EDITOR
         Core.EnumeratorLib.EnumeratorPlayer m_Player = null;
-        string m_Res = "";
-        int m_Times = 0;
+
         [UCL.Core.ATTR.UCL_FunctionButton]
-        public void EditorCoroutine() {
-            //Core.EditorLib.UCL_EditorCoroutineManager.StartCoroutine(WebRequestLoadBundle(DownloadPath, SavePath));
+        public void EditorDownload() {
+            EditorStopCoroutine();
+            m_Player = Core.EditorLib.UCL_EditorCoroutineManager.StartCoroutine(WebRequestLoad(DownloadPath, SavePath));
+        }
+        [UCL.Core.ATTR.UCL_FunctionButton]
+        public void EditorStopCoroutine() {
             Core.EditorLib.UCL_EditorCoroutineManager.StopCoroutine(m_Player);
-            m_Player = Core.EditorLib.UCL_EditorCoroutineManager.StartCoroutine(test_num());
-            //if(m_Player != null) {
-            //    m_Player = null;
-            //    Core.EditorLib.UCL_EditorUpdateManager.RemoveEditorUpdateAct(UpdateAct);
-            //}
-            //m_Res = "";
-            //m_Times = 0;
-            //m_Player = Core.EnumeratorLib.EnumeratorPlayer.Play(WebRequestLoadBundle(DownloadPath, SavePath));//test()
-            //Core.EditorLib.UCL_EditorUpdateManager.AddEditorUpdateAct(UpdateAct);
-        }
-        void UpdateAct() {
-            if(m_Player != null) {
-                var res = m_Player.Update();
-                m_Times++;
-                if(res != null) {
-                    m_Res += res;
-                    Debug.LogWarning(m_Times.ToString("00000") + "result:" + m_Res);
-                } else {
-                    //Debug.LogWarning(m_Times.ToString("00000") + "result == null");
-                }
-
-                if(m_Player.End()) {
-                    m_Player = null;
-                    Core.EditorLib.UCL_EditorUpdateManager.RemoveEditorUpdateAct(UpdateAct);
-                }
-            }
-        }
-        private void Update() {
-
-        }
-        IEnumerator test_num() {
-            for(int i = 0; i < 10; i++) {
-                //Debug.LogWarning("pre i:" + i + ",time:" + System.DateTime.Now.ToString("ss.f"));
-                
-                Debug.LogWarning("i:" + i + ",time:" + System.DateTime.Now.ToString("ss.f"));
-                yield return Core.EnumeratorLib.Wait.WaitSeconds(0.5f + 0.5f * i);
-            }
-        }
-        IEnumerator test() {
-            for(int i = 0; i < 10; i++) {
-                yield return test2(i);
-                yield return System.Environment.NewLine;
-                yield return Core.EnumeratorLib.Wait.WaitSeconds(1.5f);
-            }
-        }
-        IEnumerator test2(int j) {
-            for(int i = j; i < 10; i++) {
-                yield return i.ToString()+",";
-                yield return Core.EnumeratorLib.Wait.WaitForUpdate(9);
-                //yield return test3(i);
-            }
-        }
-        IEnumerator test3(int j) {
-            for(int i = j; i < 3; i++) {
-                yield return char.ToString((char)('A'+i)) + ",";
-            }
         }
 #endif
     }
