76351d6479d6da7f0376c9117d64e37e79f68b95
tim099 <tim11251994@gmail.com>
2021-01-19 17:38:50 +0800

    Update UCL_FileDownloader

--- UCL_NetworkScript/UCL_FileDownloader.cs
+++ UCL_NetworkScript/UCL_FileDownloader.cs
@@ -17,11 +17,12 @@ namespace UCL.NetworkLib {
 
         [UCL.Core.ATTR.UCL_FunctionButton]
         public void StartDownload() {
+#if UNITY_EDITOR
             if(string.IsNullOrEmpty(m_SaveFolder)) {
                 var path = UnityEditor.AssetDatabase.GetAssetPath(this);
                 m_SaveFolder = Core.FileLib.Lib.RemoveFolderPath(path, 1);
-                
             }
+#endif
             UCL.Core.EnumeratorLib.UCL_CoroutineManager.StartCoroutine(WebRequestDownload(m_DownloadPath, delegate(byte[] data) {
                 File.WriteAllBytes(SavePath, data);
 #if UNITY_EDITOR
